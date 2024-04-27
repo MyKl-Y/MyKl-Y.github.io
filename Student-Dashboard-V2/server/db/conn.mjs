756c3137dbcad57f4306daa94b10906754edcccf
MyKl-Y <michaelyyim@gmail.com>
2024-04-27 00:34:59 -0400

    Test

--- Student-Dashboard-V2/server/db/conn.mjs
+++ Student-Dashboard-V2/server/db/conn.mjs
@@ -5,13 +5,15 @@ const connectionString = process.env.ATLAS_URI || "";
 const client = new MongoClient(connectionString);
 
 let conn;
+let db;
 try {
     conn = await client.connect();
+    db = conn.db("student_dashboard");
+    console.log(`Successfully connected to MongoDB.`);
 } catch(e) {
     console.error(e);
 }
 
-let db = conn.db("student_dashboard");
-console.log(`Successfully connected to MongoDB.`);
+
 
 export default db;
