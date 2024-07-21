f2204415063f6f83465d63988c072ab03167322b
MyKl-Y <michaelyyim@gmail.com>
2024-07-20 22:36:05 -0400

    Test 1

---
+++ Portfolio-Site/client/vite.config.ts
@@ -0,0 +1,29 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { resolve } from 'node:path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/Portfolio-Site/', // Base URL for the app
+  server: {
+    port: 5173 // Development server port
+  },
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)) // Alias for src directory
+    }
+  },
+  build: {
+    outDir: 'dist', // Output directory
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html') // Entry point for the build
+    }
+  }
+})
